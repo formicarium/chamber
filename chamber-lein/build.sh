75003e0ef0627ecedd0e3e79164dd08d37d6ab2a
Rafael Leal <rafaelealdias@gmail.com>
2018-08-10 23:09:14 -0300

    Chamber-lein version

--- chamber-lein/build.sh
+++ chamber-lein/build.sh
@@ -4,4 +4,6 @@ set -eou pipefail
 
 docker build -t chamber-lein:$1 .
 docker tag chamber-lein:$1 formicarium/chamber-lein:$1
+docker tag chamber-lein:$1 formicarium/chamber-lein:latest
 docker push formicarium/chamber-lein:$1
+docker push formicarium/chamber-lein:latest
